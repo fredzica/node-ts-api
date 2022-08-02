7f39edf9e654e319df964f6c1cf88c3b692109e4
Frederico Zica <fredzica@gmail.com>
2022-08-02 09:35:59 -0300

    separated modules for controllers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 
+import main from './controllers/main'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/main', main)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
